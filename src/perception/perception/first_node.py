31bfc6a3a9fbea4b166aa07892f0ebef31150a6a
Hektor Buqaj <hektorb@umich.edu>
2025-11-21 17:38:57 +0000

    updated node

--- src/perception/perception/first_node.py
+++ src/perception/perception/first_node.py
@@ -7,6 +7,10 @@ class myNode(Node):
     def __init__(self, name):
         super().__init__(name) # Calling the inherited class's ctor
         self.get_logger().info("Hello from ROS2!")
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("Hello")
 
 def main(args=None):
     rclpy.init(args=args) # Start ros2 communications 
